abcf4ab37c1bd3994aa6057abe56e4479284b679
Miguel <miguelcamargo9@gmail.com>
2019-06-22 16:42:14 -0500

    Switch routes by profile

--- src/routes.js
+++ src/routes.js
@@ -20,137 +20,418 @@ import Gavel from "@material-ui/icons/Gavel";
 import CardTravel from "@material-ui/icons/CardTravel";
 import Lock from "@material-ui/icons/Lock";
 
-var dashRoutes = [
-  {
-    path: "/home",
-    name: "Inicio",
-    rtlName: "Home",
-    icon: HomeIcon,
-    component: Home,
-    layout: "/admin"
-  },
-  {
-    collapse: true,
-    name: "Administración",
-    rtlName: "Admin",
-    icon: Lock,
-    views: [
-      {
-        path: "/admin-slides",
-        name: "Slides",
-        rtlName: "Admin Slides",
-        mini: "SL",
-        rtlMini: "SL",
-        layout: "/admin",
-        component: AdminSlide
-      }
-    ]
-  },
-  {
-    collapse: true,
-    name: "Usuarios",
-    rtlName: "Users",
-    icon: Users,
-    state: "pageCollapse",
-    views: [
-      {
-        path: "/create-user",
-        name: "Crear Usuario",
-        rtlName: "Create User",
-        mini: "CU",
-        rtlMini: "CU",
-        component: RegisterUserForm,
-        layout: "/admin"
-      },
-      {
-        path: "/list-users",
-        name: "Lista Usuarios",
-        rtlName: "User List",
-        mini: "LU",
-        rtlMini: "UL",
-        component: UserList,
-        layout: "/admin"
-      }
-    ]
-  },
-  {
-    collapse: true,
-    name: "Ventas",
-    rtlName: "Sales",
-    icon: CardTravel,
-    state: "pageSaleCollapse",
-    views: [
-      {
-        path: "/register-sale",
-        name: "Ingresar Venta",
-        rtlName: "Register Sale",
-        mini: "IV",
-        rtlMini: "RS",
-        component: RegisterInvoiceForm,
-        layout: "/admin"
-      },
-      {
-        path: "/approve-change",
-        name: "Aprobar Premio",
-        rtlName: "Approve Change",
-        mini: "AP",
-        rtlMini: "AC",
-        component: CreateUserForm,
-        layout: "/admin"
-      }
-    ]
-  },
-  {
-    path: "/products",
-    name: "Catálogo de Productos",
-    rtlName: "Products",
-    icon: Gift,
-    component: ProductList,
-    layout: "/admin"
-  },
-  {
-    path: "/howtoworks",
-    name: "Cómo Funciona",
-    rtlName: "How To Works",
-    icon: TouchApp,
-    component: HowToWorks,
-    layout: "/admin"
-  },
-  {
-    path: "/contact",
-    name: "Contáctanos",
-    rtlName: "Contact us",
-    icon: Email,
-    component: ContactForm,
-    layout: "/admin"
-  },
-  {
-    path: "/politics",
-    name: "Políticas Y Condiciones",
-    rtlName: "Politics",
-    icon: Gavel,
-    component: Politics,
-    layout: "/admin"
-  },
-  {
-    path: "/register-page",
-    name: "Registrar Usuario",
-    rtlName: "User Register",
-    mini: "RU",
-    rtlMini: "UR",
-    component: RegisterUserForm,
-    layout: "/auth",
-    invisible: true
-  },
-  {
-    path: "/login-page",
-    name: "Iniciar Sesión",
-    rtlName: "Login Page",
-    mini: "L",
-    rtlMini: "LP",
-    component: LoginPage,
-    layout: "/auth",
-    invisible: true
+if (localStorage.getItem("profile")) {
+  switch (localStorage.getItem("profile")) {
+    case 1: {
+      var dashRoutes = [
+        {
+          path: "/home",
+          name: "Inicio",
+          rtlName: "Home",
+          icon: HomeIcon,
+          component: Home,
+          layout: "/admin"
+        },
+        {
+          collapse: true,
+          name: "Administración",
+          rtlName: "Admin",
+          icon: Lock,
+          views: [
+            {
+              path: "/admin-slides",
+              name: "Slides",
+              rtlName: "Admin Slides",
+              mini: "SL",
+              rtlMini: "SL",
+              layout: "/admin",
+              component: AdminSlide
+            }
+          ]
+        },
+        {
+          collapse: true,
+          name: "Usuarios",
+          rtlName: "Users",
+          icon: Users,
+          state: "pageCollapse",
+          views: [
+            {
+              path: "/create-user",
+              name: "Crear Usuario",
+              rtlName: "Create User",
+              mini: "CU",
+              rtlMini: "CU",
+              component: RegisterUserForm,
+              layout: "/admin"
+            },
+            {
+              path: "/list-users",
+              name: "Lista Usuarios",
+              rtlName: "User List",
+              mini: "LU",
+              rtlMini: "UL",
+              component: UserList,
+              layout: "/admin"
+            }
+          ]
+        },
+        {
+          collapse: true,
+          name: "Ventas",
+          rtlName: "Sales",
+          icon: CardTravel,
+          state: "pageSaleCollapse",
+          views: [
+            {
+              path: "/register-sale",
+              name: "Ingresar Venta",
+              rtlName: "Register Sale",
+              mini: "IV",
+              rtlMini: "RS",
+              component: RegisterInvoiceForm,
+              layout: "/admin"
+            },
+            {
+              path: "/approve-change",
+              name: "Aprobar Premio",
+              rtlName: "Approve Change",
+              mini: "AP",
+              rtlMini: "AC",
+              component: CreateUserForm,
+              layout: "/admin"
+            }
+          ]
+        },
+        {
+          path: "/products",
+          name: "Catálogo de Productos",
+          rtlName: "Products",
+          icon: Gift,
+          component: ProductList,
+          layout: "/admin"
+        },
+        {
+          path: "/howtoworks",
+          name: "Cómo Funciona",
+          rtlName: "How To Works",
+          icon: TouchApp,
+          component: HowToWorks,
+          layout: "/admin"
+        },
+        {
+          path: "/contact",
+          name: "Contáctanos",
+          rtlName: "Contact us",
+          icon: Email,
+          component: ContactForm,
+          layout: "/admin"
+        },
+        {
+          path: "/politics",
+          name: "Políticas Y Condiciones",
+          rtlName: "Politics",
+          icon: Gavel,
+          component: Politics,
+          layout: "/admin"
+        },
+        {
+          path: "/register-page",
+          name: "Registrar Usuario",
+          rtlName: "User Register",
+          mini: "RU",
+          rtlMini: "UR",
+          component: RegisterUserForm,
+          layout: "/auth",
+          invisible: true
+        },
+        {
+          path: "/login-page",
+          name: "Iniciar Sesión",
+          rtlName: "Login Page",
+          mini: "L",
+          rtlMini: "LP",
+          component: LoginPage,
+          layout: "/auth",
+          invisible: true
+        }
+      ];
+    }
+    case 4: {
+      var dashRoutes = [
+        {
+          path: "/home",
+          name: "Inicio",
+          rtlName: "Home",
+          icon: HomeIcon,
+          component: Home,
+          layout: "/admin"
+        },
+        {
+          collapse: true,
+          name: "Ventas",
+          rtlName: "Sales",
+          icon: CardTravel,
+          state: "pageSaleCollapse",
+          views: [
+            {
+              path: "/approve-change",
+              name: "Aprobar Premio",
+              rtlName: "Approve Change",
+              mini: "AP",
+              rtlMini: "AC",
+              component: CreateUserForm,
+              layout: "/admin"
+            }
+          ]
+        },
+        {
+          path: "/products",
+          name: "Catálogo de Productos",
+          rtlName: "Products",
+          icon: Gift,
+          component: ProductList,
+          layout: "/admin"
+        },
+        {
+          path: "/register-page",
+          name: "Registrar Usuario",
+          rtlName: "User Register",
+          mini: "RU",
+          rtlMini: "UR",
+          component: RegisterUserForm,
+          layout: "/auth",
+          invisible: true
+        },
+        {
+          path: "/login-page",
+          name: "Iniciar Sesión",
+          rtlName: "Login Page",
+          mini: "L",
+          rtlMini: "LP",
+          component: LoginPage,
+          layout: "/auth",
+          invisible: true
+        }
+      ];
+    }
+    default: {
+      var dashRoutes = [
+        {
+          path: "/home",
+          name: "Inicio",
+          rtlName: "Home",
+          icon: HomeIcon,
+          component: Home,
+          layout: "/admin"
+        },
+        {
+          collapse: true,
+          name: "Ventas",
+          rtlName: "Sales",
+          icon: CardTravel,
+          state: "pageSaleCollapse",
+          views: [
+            {
+              path: "/register-sale",
+              name: "Ingresar Venta",
+              rtlName: "Register Sale",
+              mini: "IV",
+              rtlMini: "RS",
+              component: RegisterInvoiceForm,
+              layout: "/admin"
+            }
+          ]
+        },
+        {
+          path: "/products",
+          name: "Catálogo de Productos",
+          rtlName: "Products",
+          icon: Gift,
+          component: ProductList,
+          layout: "/admin"
+        },
+        {
+          path: "/howtoworks",
+          name: "Cómo Funciona",
+          rtlName: "How To Works",
+          icon: TouchApp,
+          component: HowToWorks,
+          layout: "/admin"
+        },
+        {
+          path: "/contact",
+          name: "Contáctanos",
+          rtlName: "Contact us",
+          icon: Email,
+          component: ContactForm,
+          layout: "/admin"
+        },
+        {
+          path: "/politics",
+          name: "Políticas Y Condiciones",
+          rtlName: "Politics",
+          icon: Gavel,
+          component: Politics,
+          layout: "/admin"
+        },
+        {
+          path: "/register-page",
+          name: "Registrar Usuario",
+          rtlName: "User Register",
+          mini: "RU",
+          rtlMini: "UR",
+          component: RegisterUserForm,
+          layout: "/auth",
+          invisible: true
+        },
+        {
+          path: "/login-page",
+          name: "Iniciar Sesión",
+          rtlName: "Login Page",
+          mini: "L",
+          rtlMini: "LP",
+          component: LoginPage,
+          layout: "/auth",
+          invisible: true
+        }
+      ];
+    }
   }
-];
+} else {
+  var dashRoutes = [
+    {
+      path: "/home",
+      name: "Inicio",
+      rtlName: "Home",
+      icon: HomeIcon,
+      component: Home,
+      layout: "/admin"
+    },
+    {
+      collapse: true,
+      name: "Administración",
+      rtlName: "Admin",
+      icon: Lock,
+      views: [
+        {
+          path: "/admin-slides",
+          name: "Slides",
+          rtlName: "Admin Slides",
+          mini: "SL",
+          rtlMini: "SL",
+          layout: "/admin",
+          component: AdminSlide
+        }
+      ]
+    },
+    {
+      collapse: true,
+      name: "Usuarios",
+      rtlName: "Users",
+      icon: Users,
+      state: "pageCollapse",
+      views: [
+        {
+          path: "/create-user",
+          name: "Crear Usuario",
+          rtlName: "Create User",
+          mini: "CU",
+          rtlMini: "CU",
+          component: RegisterUserForm,
+          layout: "/admin"
+        },
+        {
+          path: "/list-users",
+          name: "Lista Usuarios",
+          rtlName: "User List",
+          mini: "LU",
+          rtlMini: "UL",
+          component: UserList,
+          layout: "/admin"
+        }
+      ]
+    },
+    {
+      collapse: true,
+      name: "Ventas",
+      rtlName: "Sales",
+      icon: CardTravel,
+      state: "pageSaleCollapse",
+      views: [
+        {
+          path: "/register-sale",
+          name: "Ingresar Venta",
+          rtlName: "Register Sale",
+          mini: "IV",
+          rtlMini: "RS",
+          component: RegisterInvoiceForm,
+          layout: "/admin"
+        },
+        {
+          path: "/approve-change",
+          name: "Aprobar Premio",
+          rtlName: "Approve Change",
+          mini: "AP",
+          rtlMini: "AC",
+          component: CreateUserForm,
+          layout: "/admin"
+        }
+      ]
+    },
+    {
+      path: "/products",
+      name: "Catálogo de Productos",
+      rtlName: "Products",
+      icon: Gift,
+      component: ProductList,
+      layout: "/admin"
+    },
+    {
+      path: "/howtoworks",
+      name: "Cómo Funciona",
+      rtlName: "How To Works",
+      icon: TouchApp,
+      component: HowToWorks,
+      layout: "/admin"
+    },
+    {
+      path: "/contact",
+      name: "Contáctanos",
+      rtlName: "Contact us",
+      icon: Email,
+      component: ContactForm,
+      layout: "/admin"
+    },
+    {
+      path: "/politics",
+      name: "Políticas Y Condiciones",
+      rtlName: "Politics",
+      icon: Gavel,
+      component: Politics,
+      layout: "/admin"
+    },
+    {
+      path: "/register-page",
+      name: "Registrar Usuario",
+      rtlName: "User Register",
+      mini: "RU",
+      rtlMini: "UR",
+      component: RegisterUserForm,
+      layout: "/auth",
+      invisible: true
+    },
+    {
+      path: "/login-page",
+      name: "Iniciar Sesión",
+      rtlName: "Login Page",
+      mini: "L",
+      rtlMini: "LP",
+      component: LoginPage,
+      layout: "/auth",
+      invisible: true
+    }
+  ];
+}
+
 export default dashRoutes;
