ad3866f6af8e421fe663edcdfeba3771dc2368b8
Miguel <miguelcamargo9@gmail.com>
2019-06-23 12:54:16 -0500

    Change of routes for new users

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,8 @@
 // import routes
+import LoginPage from "./views/Pages/LoginPage.jsx";
+
 import { adminRoutes, defaultRoutes, sellRoutes } from "./routes/";
+import RegisterUserForm from "./views/Forms/Users/RegisterUserForm.jsx";
 
 const profile = Number.parseInt(localStorage.getItem("profile"), 10);
 
@@ -17,6 +20,29 @@ if (profile) {
       dashRoutes = [...defaultRoutes];
       break;
   }
+} else {
+  dashRoutes = [
+    {
+      path: "/login-page",
+      name: "Iniciar Sesión",
+      rtlName: "Login Page",
+      mini: "L",
+      rtlMini: "LP",
+      component: LoginPage,
+      layout: "/auth",
+      invisible: true
+    },
+    {
+      path: "/register-page",
+      name: "Registrar Usuario",
+      rtlName: "User Register",
+      mini: "RU",
+      rtlMini: "UR",
+      component: RegisterUserForm,
+      layout: "/auth",
+      invisible: true
+    }
+  ];
 }
 
 export default dashRoutes;
