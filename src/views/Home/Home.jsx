5245b79e813ac502d4b4e190cd30663c1f9eedf2
Miguel <miguelcamargo9@gmail.com>
2019-06-24 16:11:11 -0500

    List of Wheels on Home

--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -3,11 +3,16 @@ import { connect } from "react-redux";
 import { bindActionCreators } from "redux";
 import compose from "recompose/compose";
 
+// core components
+import GridContainer from "components/Grid/GridContainer.jsx";
+import GridItem from "components/Grid/GridItem.jsx";
+
 // @material-ui/core components
 import withStyles from "@material-ui/core/styles/withStyles";
 import appStyle from "assets/jss/material-dashboard-pro-react/layouts/adminStyle.jsx";
 
 import Slides from "../../components/Slides/Slides.jsx";
+import WheelsList from "../Lists/Wheels/WheelsList";
 import * as slideActions from "../../actions/slideActions";
 
 class Home extends React.Component {
@@ -15,7 +20,42 @@ class Home extends React.Component {
     this.props.SlideActions.getSlidesAction();
   }
   render() {
-    return <Slides slides={this.props.slides} />;
+    return (
+      <div>
+        <GridContainer>
+          <GridItem xs={12} sm={12}>
+            <Slides slides={this.props.slides} />
+          </GridItem>
+        </GridContainer>
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <GridContainer>
+          <GridItem xs={12} sm={12}>
+            <WheelsList />
+          </GridItem>
+        </GridContainer>
+      </div>
+    );
   }
 }
 
---
+++ src/views/Lists/Wheels/WheelsList.jsx
@@ -0,0 +1,254 @@
+import React from "react";
+
+// @material-ui/core components
+import withStyles from "@material-ui/core/styles/withStyles";
+import Tooltip from "@material-ui/core/Tooltip";
+
+// @material-ui/icons
+import ArtTrack from "@material-ui/icons/ArtTrack";
+import Refresh from "@material-ui/icons/Refresh";
+import Edit from "@material-ui/icons/Edit";
+
+// core components
+import GridContainer from "components/Grid/GridContainer.jsx";
+import GridItem from "components/Grid/GridItem.jsx";
+import Card from "components/Card/Card.jsx";
+import CardHeader from "components/Card/CardHeader.jsx";
+import CardBody from "components/Card/CardBody.jsx";
+import CardFooter from "components/Card/CardFooter.jsx";
+import Button from "components/CustomButtons/Button.jsx";
+
+import dashboardStyle from "assets/jss/material-dashboard-pro-react/views/dashboardStyle";
+
+import wheel1 from "assets/img/llantascontinental/contisportcontact5.png";
+import wheel2 from "assets/img/llantascontinental/crosscontactatr.png";
+import wheel3 from "assets/img/llantascontinental/crosscontactat50.png";
+import wheel4 from "assets/img/llantascontinental/crosscontactuhp.png";
+
+class WheelsList extends React.Component {
+  render() {
+    const { classes } = this.props;
+    return (
+      <div>
+        <GridContainer justify="space-between">
+          <GridItem xs={12} sm={2}>
+            <h4>Lista de Llantas</h4>
+          </GridItem>
+        </GridContainer>
+        <hr />
+        <GridContainer>
+          <GridItem xs={12} sm={6} md={6} lg={3}>
+            <Card product className={classes.cardHover}>
+              <CardHeader image className={classes.cardHeaderHover}>
+                <img src={wheel1} alt={wheel1} />
+              </CardHeader>
+              <CardBody>
+                <div className={classes.cardHoverUnder}>
+                  <Tooltip
+                    id="tooltip-top"
+                    title="View"
+                    placement="bottom"
+                    classes={{ tooltip: classes.tooltip }}
+                  >
+                    <Button color="transparent" simple justIcon>
+                      <ArtTrack className={classes.underChartIcons} />
+                    </Button>
+                  </Tooltip>
+                  <Tooltip
+                    id="tooltip-top"
+                    title="Edit"
+                    placement="bottom"
+                    classes={{ tooltip: classes.tooltip }}
+                  >
+                    <Button color="success" simple justIcon>
+                      <Refresh className={classes.underChartIcons} />
+                    </Button>
+                  </Tooltip>
+                  <Tooltip
+                    id="tooltip-top"
+                    title="Remove"
+                    placement="bottom"
+                    classes={{ tooltip: classes.tooltip }}
+                  >
+                    <Button color="danger" simple justIcon>
+                      <Edit className={classes.underChartIcons} />
+                    </Button>
+                  </Tooltip>
+                </div>
+                <h4 className={classes.cardProductTitle}>
+                  <a href="#pablo" onClick={e => e.preventDefault()}>
+                    Contisportcontact5
+                  </a>
+                </h4>
+                <p className={classes.cardProductDesciprion}>{"Continetal"}</p>
+              </CardBody>
+              <CardFooter product>
+                <div className={classes.price}>
+                  <h4>contisportcontact5</h4>
+                </div>
+              </CardFooter>
+            </Card>
+          </GridItem>
+          <GridItem xs={12} sm={6} md={6} lg={3}>
+            <Card product className={classes.cardHover}>
+              <CardHeader image className={classes.cardHeaderHover}>
+                <img src={wheel2} alt={wheel2} />
+              </CardHeader>
+              <CardBody>
+                <div className={classes.cardHoverUnder}>
+                  <Tooltip
+                    id="tooltip-top"
+                    title="View"
+                    placement="bottom"
+                    classes={{ tooltip: classes.tooltip }}
+                  >
+                    <Button color="transparent" simple justIcon>
+                      <ArtTrack className={classes.underChartIcons} />
+                    </Button>
+                  </Tooltip>
+                  <Tooltip
+                    id="tooltip-top"
+                    title="Edit"
+                    placement="bottom"
+                    classes={{ tooltip: classes.tooltip }}
+                  >
+                    <Button color="success" simple justIcon>
+                      <Refresh className={classes.underChartIcons} />
+                    </Button>
+                  </Tooltip>
+                  <Tooltip
+                    id="tooltip-top"
+                    title="Remove"
+                    placement="bottom"
+                    classes={{ tooltip: classes.tooltip }}
+                  >
+                    <Button color="danger" simple justIcon>
+                      <Edit className={classes.underChartIcons} />
+                    </Button>
+                  </Tooltip>
+                </div>
+                <h4 className={classes.cardProductTitle}>
+                  <a href="#pablo" onClick={e => e.preventDefault()}>
+                    crosscontactatr
+                  </a>
+                </h4>
+                <p className={classes.cardProductDesciprion}>{"Continetal"}</p>
+              </CardBody>
+              <CardFooter product>
+                <div className={classes.price}>
+                  <h4>crosscontactatr</h4>
+                </div>
+              </CardFooter>
+            </Card>
+          </GridItem>
+          <GridItem xs={12} sm={6} md={6} lg={3}>
+            <Card product className={classes.cardHover}>
+              <CardHeader image className={classes.cardHeaderHover}>
+                <img src={wheel3} alt={wheel3} />
+              </CardHeader>
+              <CardBody>
+                <div className={classes.cardHoverUnder}>
+                  <Tooltip
+                    id="tooltip-top"
+                    title="View"
+                    placement="bottom"
+                    classes={{ tooltip: classes.tooltip }}
+                  >
+                    <Button color="transparent" simple justIcon>
+                      <ArtTrack className={classes.underChartIcons} />
+                    </Button>
+                  </Tooltip>
+                  <Tooltip
+                    id="tooltip-top"
+                    title="Edit"
+                    placement="bottom"
+                    classes={{ tooltip: classes.tooltip }}
+                  >
+                    <Button color="success" simple justIcon>
+                      <Refresh className={classes.underChartIcons} />
+                    </Button>
+                  </Tooltip>
+                  <Tooltip
+                    id="tooltip-top"
+                    title="Remove"
+                    placement="bottom"
+                    classes={{ tooltip: classes.tooltip }}
+                  >
+                    <Button color="danger" simple justIcon>
+                      <Edit className={classes.underChartIcons} />
+                    </Button>
+                  </Tooltip>
+                </div>
+                <h4 className={classes.cardProductTitle}>
+                  <a href="#pablo" onClick={e => e.preventDefault()}>
+                    crosscontactat50
+                  </a>
+                </h4>
+                <p className={classes.cardProductDesciprion}>{"Continetal"}</p>
+              </CardBody>
+              <CardFooter product>
+                <div className={classes.price}>
+                  <h4>crosscontactat50</h4>
+                </div>
+              </CardFooter>
+            </Card>
+          </GridItem>
+          <GridItem xs={12} sm={6} md={6} lg={3}>
+            <Card product className={classes.cardHover}>
+              <CardHeader image className={classes.cardHeaderHover}>
+                <img src={wheel4} alt={wheel4} />
+              </CardHeader>
+              <CardBody>
+                <div className={classes.cardHoverUnder}>
+                  <Tooltip
+                    id="tooltip-top"
+                    title="View"
+                    placement="bottom"
+                    classes={{ tooltip: classes.tooltip }}
+                  >
+                    <Button color="transparent" simple justIcon>
+                      <ArtTrack className={classes.underChartIcons} />
+                    </Button>
+                  </Tooltip>
+                  <Tooltip
+                    id="tooltip-top"
+                    title="Edit"
+                    placement="bottom"
+                    classes={{ tooltip: classes.tooltip }}
+                  >
+                    <Button color="success" simple justIcon>
+                      <Refresh className={classes.underChartIcons} />
+                    </Button>
+                  </Tooltip>
+                  <Tooltip
+                    id="tooltip-top"
+                    title="Remove"
+                    placement="bottom"
+                    classes={{ tooltip: classes.tooltip }}
+                  >
+                    <Button color="danger" simple justIcon>
+                      <Edit className={classes.underChartIcons} />
+                    </Button>
+                  </Tooltip>
+                </div>
+                <h4 className={classes.cardProductTitle}>
+                  <a href="#pablo" onClick={e => e.preventDefault()}>
+                    crosscontactat50
+                  </a>
+                </h4>
+                <p className={classes.cardProductDesciprion}>{"Continetal"}</p>
+              </CardBody>
+              <CardFooter product>
+                <div className={classes.price}>
+                  <h4>crosscontactat50</h4>
+                </div>
+              </CardFooter>
+            </Card>
+          </GridItem>
+        </GridContainer>
+      </div>
+    );
+  }
+}
+
+export default withStyles(dashboardStyle)(WheelsList);
