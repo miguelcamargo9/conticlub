fea5652419104c46af1447ad62a5d763f6a7cc47
Miguel <miguelcamargo9@gmail.com>
2019-07-07 18:29:13 -0500

    Services without console.log

--- src/services/productRedeemService.js
+++ src/services/productRedeemService.js
@@ -1,46 +1,55 @@
 import axios from "axios";
+import { sessionService } from "redux-react-session";
 
 import * as serviceConst from "./index";
 import { SERVER_URL } from "../constants/server";
 
 export const approveRedeemService = redeemId => {
-  const REDEEM_API_ENDPOINT = `${SERVER_URL}/api/product/approve/${redeemId}`;
+  return sessionService
+    .loadSession()
+    .then(currentSession => {
+      const REDEEM_API_ENDPOINT = `${SERVER_URL}/api/product/approve/${redeemId}`;
 
-  const dataRedeem = {
-    headers: {
-      Authorization: serviceConst.AUTH
-    }
-  };
+      const headers = {
+        Authorization: `Bearer ${currentSession.access_token}`
+      };
 
-  return axios
-    .post(REDEEM_API_ENDPOINT, dataRedeem)
-    .then(response => {
-      console.log(response);
-      return response;
+      return axios
+        .post(REDEEM_API_ENDPOINT, null, { headers: headers })
+        .then(response => {
+          console.log(response);
+          return response;
+        })
+        .catch(function(error) {
+          console.log(error);
+        });
     })
-    .catch(function(error) {
-      console.log(error);
-    });
+    .catch(err => console.log(err));
 };
+
 export const rejectRedeemService = redeemId => {
-  const REDEEM_API_ENDPOINT = `${SERVER_URL}/api/product/reject/${redeemId}`;
+  return sessionService
+    .loadSession()
+    .then(currentSession => {
+      const REDEEM_API_ENDPOINT = `${SERVER_URL}/api/product/reject/${redeemId}`;
 
-  const dataRedeem = {
-    headers: {
-      Authorization: serviceConst.AUTH
-    }
-  };
+      const headers = {
+        Authorization: `Bearer ${currentSession.access_token}`
+      };
 
-  return axios
-    .post(REDEEM_API_ENDPOINT, dataRedeem)
-    .then(response => {
-      console.log(response);
-      return response;
+      return axios
+        .post(REDEEM_API_ENDPOINT, null, { headers: headers })
+        .then(response => {
+          console.log(response);
+          return response;
+        })
+        .catch(function(error) {
+          console.log(error);
+        });
     })
-    .catch(function(error) {
-      console.log(error);
-    });
+    .catch(err => console.log(err));
 };
+
 export const getRedeemByIdService = redeemId => {
   const REDEEM_API_ENDPOINT = `${SERVER_URL}/api/product/applyfor/get/${redeemId}`;
 
